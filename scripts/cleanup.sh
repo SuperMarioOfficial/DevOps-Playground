a71fa3035620f2b38dac7f63e0a5e5e6c790070b
cybern3tic <61300932+cybern3tic@users.noreply.github.com>
2020-03-24 21:27:13 +0100

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,46 +2,11 @@
 logz='cleanup.log'
 
 echo "##############################################################################"
-echo "# 01_Update System                                                           #" | tee -a $logz
+echo "# 02_Cleaning                                                                #"
 echo "##############################################################################"
-apt-get -y -qq update | tee -a $logz
-apt-get install resolvconf
-apt-get install net-tools
-#apt-get update --fix-missing | tee -a $logz
-#DEBIAN_FRONTEND=noninteractive apt-get upgrade -y -o Dpkg::Options::='--force-confnew' | tee -a $logz
-#DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y -o Dpkg::Options::='--force-confnew' | tee -a $logz
-#DEBIAN_FRONTEND=noninteractive apt-get autoremove -y -o Dpkg::Options::='--force-confnew' | tee -a $logz
-#DEBIAN_FRONTEND=noninteractive apt-get -y -qq dist-upgrade | tee -a $logz
-#DEBIAN_FRONTEND=noninteractive apt-get -y -qq install linux-headers-"$(uname -r)" | tee -a $logz
-
-echo "##############################################################################"
-echo "# 02_Cleaning                                                                #" | tee -a $logz
-echo "##############################################################################"
-echo"-----Delete linux-headers-----" | tee -a $logz
-dpkg --list | awk '{ print $2 }' | grep 'linux-headers' | grep -vF "$(uname -r)" | xargs apt-get -y purge | tee -a $logz
-
-echo "-----Delete linux-image-----" | tee -a $logz
-dpkg --list | awk '{ print $2 }' | grep 'linux-image' | grep -vF "$(uname -r)" | xargs apt-get -y purge | tee -a $logz
 
 echo "-----Delete residuals-----" | tee -a $logz
 dpkg -l | grep '^rc' | awk '{print $2}' | xargs dpkg --purge| tee -a $logz
-
-echo "-----Delete linux firmaware-----" | tee -a $logz
-apt-get -y purge linux-firmware | tee -a $logz
-
-echo "-----Delete X11 libraries-----" | tee -a $logz
-apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6 | tee -a $logz
-
-echo "-----Delete obsolete networking-----" | tee -a $logz
-apt-get -y purge ppp pppconfig pppoeconf | tee -a $logz
-
-echo "-----Delete oddities-----" | tee -a $logz
-apt-get -y purge popularity-contest | tee -a $logz
-apt-get -y purge installation-report | tee -a $logz
-
-echo "-----Delete Packages-----" | tee -a $logz
-apt-get -y autoremove | tee -a $logz
-apt-get -y clean | tee -a $logz
 
 echo "-----Truncate any logs that have built up during the install-----" | tee -a $logz
 find /var/log -type f -exec truncate --size=0 {} \;| tee -a $logz
@@ -59,15 +24,25 @@ echo "-----Blank netplan machine-id (DUID) so machines get unique ID generated o
 truncate -s 0 /etc/machine-id | tee -a $logz
 
 echo "-----Zero out the free space to save space in the final image-----" | tee -a $logz
-#dd if=/dev/zero of=/EMPTY bs=1M || true | tee -a $logz
-#rm -f /EMPTY | tee -a $logz
+dd if=/dev/zero of=/EMPTY bs=1M || true | tee -a $logz
+rm -f /EMPTY | tee -a $logz
 
 echo "-----clear the history so our install isn't there-----" | tee -a $logz
 export HISTSIZE=0 | tee -a $logz
 rm -f /root/.wget-hsts | tee -a $logz
 
 echo "##############################################################################"
-echo "# 04_Others                                                                  #"| tee -a $logz
+echo "# 04_Others                                                                  #"
 echo "##############################################################################"
+apt-get -y -qq update 
+apt-get install resolvconf
+apt-get install net-tools
+apt-get install openssh-server
+apt-get update --fix-missing 
+systemctl enable ssh
+systemctl enable ssh.service
+update-rc.d ssh defaults
+systemctl enable ssh.socket
 PATH=/usr/bin:/usr/sbin
-echo "root:kosint" | sudo chpasswd
+echo "root:mindwarelab" | sudo chpasswd
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
