92a1fd5d49b173afec6f1daa7fa579429544c69d
cybern3tic <61300932+cybern3tic@users.noreply.github.com>
2020-03-25 23:50:36 +0100

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -41,6 +41,8 @@ apt-get install net-tools
 apt-get install openssh-server
 systemctl enable ssh
 systemctl enable ssh.service
+systemctl enable sshd
+systemctl start sshd
 update-rc.d ssh defaults
 systemctl enable ssh.socket
 PATH=/usr/bin:/usr/sbin
