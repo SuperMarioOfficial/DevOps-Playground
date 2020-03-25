9c34588a8c61585886521c2913f38263e382ecfe
cybern3tic <61300932+cybern3tic@users.noreply.github.com>
2020-03-26 00:31:01 +0100

    Update ansible_deb.sh

--- scripts/ansible_deb.sh
+++ scripts/ansible_deb.sh
@@ -1,5 +1,3 @@
 #!/bin/sh -eux
- apt update
- apt install software-properties-common
- apt-add-repository --yes --update ppa:ansible/ansible
- apt install ansible
+apt update
+apt-get install ansible
