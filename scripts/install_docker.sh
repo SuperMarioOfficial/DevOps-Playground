4386914a6a3cf683660bb0c6f12bda301f9e7f22
Super Mario <61300932+SuperMarioOfficial@users.noreply.github.com>
2020-03-26 22:24:24 +0100

    Update install_docker.sh

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -9,7 +9,7 @@ sudo apt-get install \
     
 #wget -c https://download.docker.com/linux/debian/dists/buster/pool/stable/amd64/docker-ce_18.06.3~ce~3-0~debian_amd64.deb
 #dpkg -i docker-ce_18.06.3~ce~3-0~debian_amd64.deb
-mr -r docker-ce_18.06.3~ce~3-0~debian_amd64.deb
+rm -r docker-ce_18.06.3~ce~3-0~debian_amd64.deb
 sudo apt update
 systemctl start docker
 systemctl enable docker
