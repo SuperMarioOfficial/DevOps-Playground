69d7c831ad1ef575204193eb8dc6577bbd774c31
Super Mario <61300932+SuperMarioOfficial@users.noreply.github.com>
2020-03-26 20:58:56 +0100

    Create install_docker.sh

---
+++ scripts/install_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -eux
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get install docker-ce
+systemctl start docker
+systemctl enable docker
+sudo usermod -aG docker $USER mindwarelab
+docker run hello-world
