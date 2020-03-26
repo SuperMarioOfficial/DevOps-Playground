a7e40a7a6088177b4a8cbff500ee987bec2d6fd9
cybern3tic <61300932+cybern3tic@users.noreply.github.com>
2020-03-26 02:14:40 +0100

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -44,7 +44,6 @@ systemctl enable ssh
 systemctl enable ssh.service
 systemctl start ssh
 systemctl enable sshd
-systemctl enable sshd.service 
 systemctl start sshd
 update-rc.d ssh defaults
 systemctl enable ssh.socket
