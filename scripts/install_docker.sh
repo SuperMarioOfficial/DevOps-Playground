a5201aedee1d386e6421e0606f7eff9e27eb483c
Super Mario <61300932+SuperMarioOfficial@users.noreply.github.com>
2020-03-26 21:12:46 +0100

    Update install_docker.sh

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,10 +1,21 @@
 #!/bin/sh -eux
 
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+    
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt-get update
-sudo apt-get remove docker docker-engine docker.io
-sudo apt-get install docker-ce
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get install docker-ce docker-ce-cli containerd.io
+
 systemctl start docker
 systemctl enable docker
 sudo usermod -aG docker $USER mindwarelab
