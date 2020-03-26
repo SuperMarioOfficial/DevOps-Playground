cb11f7e64d846b847cee889ddb4b043d0af49b90
Super Mario <61300932+SuperMarioOfficial@users.noreply.github.com>
2020-03-26 20:49:34 +0100

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,7 +2,22 @@
 logz='cleanup.log'
 
 echo "##############################################################################"
-echo "# 02_Cleaning                                                                #" | tee -a $logz
+echo "# Provisioning                                                               #"| tee -a $logz
+echo "##############################################################################"
+apt-get -y -qq update  --fix-missing 
+apt-get install resolvconf
+apt-get install net-tools
+apt-get install openssh-server
+systemctl enable ssh
+systemctl enable ssh.service
+update-rc.d ssh defaults
+systemctl enable ssh.socket
+PATH=/usr/bin:/usr/sbin
+echo "root:mindwarelab" | sudo chpasswd
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+
+echo "##############################################################################"
+echo "# Cleaning                                                                #" | tee -a $logz
 echo "##############################################################################"
 
 echo "-----Delete residuals-----" | tee -a $logz
@@ -14,22 +29,6 @@ find /var/log/ -name "*.log" -exec rm -f {} \;  | tee -a $logz
 
 echo "-----clear the history so our install isn't there-----" | tee -a $logz
 export HISTSIZE=0 | tee -a $logz
+unset HISTFIL
 rm -f /root/.wget-hsts | tee -a $logz
-
-echo "##############################################################################"
-echo "# 04_Others                                                                  #"| tee -a $logz
-echo "##############################################################################"
-apt-get -y -qq update  --fix-missing 
-apt-get install resolvconf
-apt-get install net-tools
-apt-get install openssh-server
-apt-get update --fix-missing 
-systemctl enable ssh
-systemctl enable ssh.service
-update-rc.d ssh defaults
-systemctl enable ssh.socket
-PATH=/usr/bin:/usr/sbin
-echo "root:mindwarelab" | sudo chpasswd
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-sh -c "echo 'mindwarelab ALL=NOPASSWD: ALL' >> /etc/sudoers"
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+ln -s /dev/null ~/.bash_history
