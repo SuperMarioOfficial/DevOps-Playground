761596db51300f8f6572e5a8e92a49bf0009a98d
Super Mario <61300932+SuperMarioOfficial@users.noreply.github.com>
2020-04-04 02:14:25 +0200

    Update virtualbox.sh

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -eux
 
+apt-get install linux-headers-$(uname -r) build-essential dkms
 wget http://download.virtualbox.org/virtualbox/4.3.8/VBoxGuestAdditions_4.3.8.iso
 mkdir /media/VBoxGuestAdditions
 mount -o loop,ro VBoxGuestAdditions_4.3.8.iso /media/VBoxGuestAdditions
