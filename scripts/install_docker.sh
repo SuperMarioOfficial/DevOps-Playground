635d00a84ecd747d74ac368eb45679214fd3bdb5
Super Mario <61300932+SuperMarioOfficial@users.noreply.github.com>
2020-04-26 16:30:19 +0200

    Update install_docker.sh

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -eux
 
+apt update
 apt-get install \
     apt-transport-https -y \
     ca-certificates -y\
@@ -7,8 +8,6 @@ apt-get install \
     gnupg2 -y\
     software-properties-common -y
     
-apt update
-apt -y install curl gnupg2 apt-transport-https software-properties-common ca-certificates
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" | sudo tee  /etc/apt/sources.list.d/docker.list
 apt install docker-ce docker-ce-cli containerd.io
